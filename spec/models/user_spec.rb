90f251109219251021e8d70b5ea28147154c036a
Lori Baumgartner <lori.burleson@gmail.com>
2017-01-24 14:53:47 -0500

    add email validation spec for users

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -34,4 +34,23 @@ RSpec.describe User, :type => :model do
     expect(@user).to_not be_valid
   end
 
+  it 'email is not too long' do
+    @user.email = 'a' * 256
+
+    expect(@user).to_not be_valid
+  end
+
+  it 'password is not too short' do
+    @user.password_digest = 'a' * 5
+
+    expect(@user).to_not be_valid
+  end
+
+  it 'email validation should reject invalid addresses' do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example. foo@barr_baz.com foo@bar+baz.com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      expect(@user).to_not be_valid
+    end
+  end
 end
