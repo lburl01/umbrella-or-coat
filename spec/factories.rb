07ad0dee32b8ad719aea560a8716f400c1d613ad
Lori Baumgartner <lori.burleson@gmail.com>
2017-01-24 15:13:00 -0500

    add zipcode model validation spec

--- spec/factories.rb
+++ spec/factories.rb
@@ -4,4 +4,10 @@ FactoryGirl.define do
     email 'lori@example.com'
     password_digest 'password'
   end
+
+  factory :zip_code do
+    user_id 1
+    zip_code 27617
+    default_zip true
+  end
 end
